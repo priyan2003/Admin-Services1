d0bfc7c36069754ec2399ae3a94c5e69dd595da2
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2024-07-13 16:12:08 +0530

    Feat: Added routing structure for versioned api for problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const express = require(`express`);
 
 const {PORT} = require(`./config/server.config`);
-
+const apiRouter = require('./routes')
 const app = express();
 
-app.get('/home',(req,res)=>{
-    return res.json({msg: "Ok"});
-})
+app.use('/api',apiRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server started at ${PORT}`);
