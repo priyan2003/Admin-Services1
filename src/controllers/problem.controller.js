47463588d4a5fc1e20c61ab0bb1bbfe21dc8e1ab
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2024-07-14 12:09:29 +0530

    Feat: added unimplemented error for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -2,26 +2,36 @@ function pingProblemController(req, res){
     return res.json({message: 'Ping controller is up'});
 }
 function addProblem(req,res){
-
+     return res.status(501).json({
+        message:'Not implemented'
+     });
 }
 
 
 function getProblem(req,res){
-
+    return res.status(501).json({
+        message:'Not implemented'
+     });
 }
 
 
 function getProblems(req,res){
-
+    return res.status(501).json({
+        message:'Not implemented'
+     });
 }
 
 
 function deleteProblem(req,res){
-
+    return res.status(501).json({
+        message:'Not implemented'
+     });
 }
 
 function updateProblem(req,res){
-    
+    return res.status(501).json({
+        message:'Not implemented'
+     });
 }
 
 module.exports = {
